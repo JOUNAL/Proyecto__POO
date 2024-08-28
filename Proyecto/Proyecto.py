18bbdcd2721f82cbb0a2c6a5f1516d4319e37141
Juan Carlos Oliveros Garzon <136606554+JOUNAL@users.noreply.github.com>
2024-08-28 14:47:37 -0500

    Update Proyecto.py

--- Proyecto/Proyecto.py
+++ Proyecto/Proyecto.py
@@ -3,7 +3,4 @@ import packages.negatives as ng
 abstract=input("Digite la funcion a evaluar:")
 Funcion1=MethodsNumerics(desired_function=abstract)
 print(Funcion1.inputed_function)
-print(Funcion1.something(x=2))
-
-
 print(ng.negative_value(Funcion1))
