5ffdb63a567aa823efdaede6d49cfa60d58ccb29
Juan Carlos Oliveros Garzon <136606554+JOUNAL@users.noreply.github.com>
2024-08-28 14:42:16 -0500

    Update negatives.py

--- Proyecto/packages/negatives.py
+++ Proyecto/packages/negatives.py
@@ -1,5 +1,3 @@
-from base_function import input_function
-
 i=-1000
 numbers=[]
 
@@ -7,6 +5,8 @@ while i <= 1000:
     numbers.append(i)
     i+=1
 
-print(numbers)
-def negative_value():
-    return 
+def negative_value(funcion_deseada):
+    for a in numbers:
+        g=funcion_deseada.something(x=a)
+        if  g <= 0 or g < 0 :
+            return g,a
