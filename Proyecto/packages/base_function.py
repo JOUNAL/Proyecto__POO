947a6eb44878ed3fe5adb99554f9565b9cf12bff
Juan Carlos Oliveros Garzon <136606554+JOUNAL@users.noreply.github.com>
2024-09-16 22:36:09 -0500

    Update base_function.py

--- Proyecto/packages/base_function.py
+++ Proyecto/packages/base_function.py
@@ -1,11 +1,17 @@
+import packages.negatives as neg
+
 class MethodsNumerics :
     def __init__(self,desired_function) -> None:
         self.inputed_function=desired_function
+        self.cut_points=0
 
     def something(self,x):
         deastract=compile(self.inputed_function,"<string>","eval")
         output_function=eval(deastract)
         return output_function
+    
+    def cut_points_set(self,a):
+        self.cut_points=a
 
     
 
